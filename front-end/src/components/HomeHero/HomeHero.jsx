777975e6020c89e77b9a3babcda437bfaa5f5e67
JoseValenciaCodes <jbg1234@protonmail.com>
2025-02-18 11:53:39 -0400

    Fixed Mermbership Link

--- front-end/src/components/HomeHero/HomeHero.jsx
+++ front-end/src/components/HomeHero/HomeHero.jsx
@@ -8,7 +8,7 @@ function HomeHero({ heroTypeClass, heroBgColor1, heroBgColor2, btnBgColor1, btnB
             <div className="container mx-auto text-center">
                 <h1 className="text-4xl font-bold mb-4">{title}</h1>
                 <p className="text-lg mb-8">{content}</p>
-                <Link to="/membership" className={`btn-primary ${btnBgColor1} hover:${btnBgHoverColor} ${btnColor} py-2 px-4 rounded`}>
+                <Link to="/memberships" className={`btn-primary ${btnBgColor1} hover:${btnBgHoverColor} ${btnColor} py-2 px-4 rounded`}>
                     <span className="text-white">{btnText}</span>
                 </Link>
             </div>
